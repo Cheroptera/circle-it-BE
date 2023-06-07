40a480d90b0bb283246a7f534504372af9cdb19c
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-07 12:11:03 +0200

    tested adding some middlewares

--- server.js
+++ server.js
@@ -17,7 +17,13 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors({ origin: ['http://localhost:3000', 'https://imaginative-churros-e76935.netlify.app'] }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, https://imaginative-churros-e76935.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-RapidAPI-Key');
+  next();
+});
+app.use(cors());
 app.use(express.json());
 
 const { Schema } = mongoose;
