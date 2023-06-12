1a7926924fd9e89365ee57f4c6f994401049452e
Frida M. Gustavson <f.munther.gustavson@gmail.com>
2023-06-13 00:41:42 +0200

    trying to solve cors problem

--- server.js
+++ server.js
@@ -17,11 +17,12 @@ const port = process.env.PORT || 8080
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     'Access-Control-Allow-Origin',
-//     'http://localhost:3000', 'http://localhost:3000/set-timer', 'https://imaginative-churros-e76935.netlify.app'
-//   )
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'http://localhost:3000', '*', 'https://imaginative-churros-e76935.netlify.app'
+  )
+})
 //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
 //   res.setHeader(
 //     'Access-Control-Allow-Headers',
