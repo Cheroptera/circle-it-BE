144db807ac7da6e376e38c90601fa1c8b26575b6
Vio Szabo <vio@vioszabo.se>
2023-06-22 12:43:01 +0200

    delete favorite workout

--- Routes/mongo-favorites.js
+++ Routes/mongo-favorites.js
@@ -10,13 +10,13 @@ mongoose.Promise = Promise
 
 //* Save favorite workouts
 router.patch('/favorites', authenticateUser, async (req, res) => {
-  const { timestamp, exercises } = req.body
-  const accessToken = req.header("Authorization")
+  const { favoriteName, timestamp, exercises } = req.body
+  const accessToken = req.header('Authorization')
 
   try {
     const user = await User.findOne({ accessToken: accessToken })
     if (user) {
-      user.favoriteWorkouts.push({ timestamp, exercises })
+      user.favoriteWorkouts.push({ favoriteName, timestamp, exercises })
       await user.save()
       res.status(201).json({
         success: true,
@@ -39,63 +39,100 @@ router.patch('/favorites', authenticateUser, async (req, res) => {
 
 //* Get favorite workouts
 router.get('/favorites', authenticateUser, async (req, res) => {
-  const accessToken = req.header("Authorization") // Retrieve the accessToken from the request headers
+  const accessToken = req.header('Authorization') // Retrieve the accessToken from the request headers
   try {
     const user = await User.findOne({ accessToken: accessToken })
     if (user) {
       res.status(200).json({
         success: true,
-        response: user.favoriteWorkouts,
+        response: user.favoriteWorkouts
       })
     } else {
       res.status(404).json({
         success: false,
-        response: 'User not found',
+        response: 'User not found'
       })
     }
   } catch (error) {
     res.status(500).json({
       success: false,
-      response: error.message,
+      response: error.message
     })
   }
 })
 
 // Change name of the favorite
-router.patch('/favorites/:favoriteid/update', authenticateUser, async (req, res) => {
-  const { favoriteid } = req.params; // Retrieve the favorite workout ID from the request parameters
-  const { favoriteName } = req.body;
-  const accessToken = req.header("Authorization");
+router.patch(
+  '/favorites/:favoriteid/update',
+  authenticateUser,
+  async (req, res) => {
+    const { favoriteid } = req.params
+    const { favoriteName } = req.body
+    const accessToken = req.header('Authorization')
+
+    try {
+      const user = await User.findOneAndUpdate(
+        {
+          accessToken: accessToken,
+          'favoriteWorkouts._id': favoriteid
+        },
+        { $set: { 'favoriteWorkouts.$.favoriteName': favoriteName } },
+        { new: true }
+      )
+
+      if (user) {
+        res.status(200).json({
+          success: true,
+          response: 'Workout name updated successfully'
+        })
+      } else {
+        res.status(404).json({
+          success: false,
+          response: 'User not found or workout not found'
+        })
+      }
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({
+        success: false,
+        response: error
+      })
+    }
+  }
+)
+
+// Delete a favorite workout
+router.delete('/favorites/:favoriteid', authenticateUser, async (req, res) => {
+  const { favoriteid } = req.params
+  const accessToken = req.header('Authorization')
 
   try {
     const user = await User.findOneAndUpdate(
       {
-        accessToken: accessToken,
-        "favoriteWorkouts._id": favoriteid, // Find the user by accessToken and the workout by its ID
+        accessToken: accessToken
       },
-      { $set: { "favoriteWorkouts.$.favoriteName": favoriteName } }, // Update the workout's name
-      { new: true } // Return the modified user document
-    );
+      { $pull: { favoriteWorkouts: { _id: favoriteid } } },
+      { new: true }
+    )
 
     if (user) {
       res.status(200).json({
         success: true,
-        response: 'Workout name updated successfully'
-      });
+        response: 'Workout deleted successfully'
+      })
     } else {
       res.status(404).json({
         success: false,
         response: 'User not found or workout not found'
-      });
+      })
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
     res.status(500).json({
       success: false,
       response: error
-    });
+    })
   }
-});
-
+})
 
 export default router
